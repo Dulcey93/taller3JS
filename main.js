613690583a67df0c81c73f42ae2b7f219d0c095d
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-16 09:26:44 -0500

    feat(Add code): :sparkles: wrong changes

--- main.js
+++ main.js
@@ -1,45 +1,67 @@
-let myFormularioSedes = document.querySelector("#myFormularioSedes");
-let myFormularioTrainers = document.querySelector("#myFormularioTrainers");
-let myFormularioCampers = document.querySelector("#myFormularioCampers");
-let myFormularioNiveles = document.querySelector("#myFormularioNiveles");
-let myFormularioRoadmaps = document.querySelector("#myFormularioRoadmaps");
-let campus = {};
+const campus = {
+  sedes: [],
+  trainers: [],
+  campers: [],
+  niveles: [],
+  roadmaps: [],
+  addSede: function(sede) {
+    this.sedes.push(sede);
+  },
+  addTrainer: function(trainer) {
+    this.trainers.push(trainer);
+  },
+  addCamper: function(camper) {
+    this.campers.push(camper);
+  },
+  addNivel: function(nivel) {
+    this.niveles.push(nivel);
+  },
+  addRoadmap: function(roadmap) {
+    this.roadmaps.push(roadmap);
+  },
+};
 
-myFormularioSedes.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    let data = Object.fromEntries(new FormData(e.target));
-    campus[`${data.nombreSede}`] = {Trainers: [], Campers: [], Niveles: [], Roadmaps: []};
-    listaSedes();
-    myFormularioSedes.reset();
-})
+// Datos de contacto de las sedes de CAMPUS
+const formularioSedes = document.querySelector('#myFormularioSedes');
+formularioSedes.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const data = Object.fromEntries(new FormData(e.target));
+  campus.addSede(data);
+  console.log(campus.sedes);
+});
 
-let listaSedes = ()=>{
-    let opciones = document.querySelector("[name='sede']");
-    opciones.innerHTML = null;
-    Object.entries(campus).forEach((id, val) => {
-        opciones.insertAdjacentHTML("beforeend", `
-            <option value="${id}">${id}</option>
-        `);
-    });
-}
+// Datos de los Trainers
+const formularioTrainers = document.querySelector('#myFormularioTrainers');
+formularioTrainers.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const data = Object.fromEntries(new FormData(e.target));
+  campus.addTrainer(data);
+  console.log(campus.trainers);
+});
 
-myFormularioTrainers.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    let data = Object.fromEntries(new FormData(e.target));
-    let sede = data.sede;
-    delete data.sede;
-    campus[`${sede}`]["Trainers"].unshift(data);
-    console.log(campus);
-    listaTeams();
-    myFormularioTrainers.reset();
-})
+// Datos de los Campers
+const formularioCampers = document.querySelector('#myFormularioCampers');
+formularioCampers.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const data = Object.fromEntries(new FormData(e.target));
+  campus.addCamper(data);
+  console.log(campus.campers);
+});
 
-let listaTeams = ()=>{
-    let opciones = document.querySelector("[name='sede']");
-    opciones.innerHTML = null;
-    Object.entries(campus).forEach((id, val) => {
-        opciones.insertAdjacentHTML("beforeend", `
-            <option value="${id}">${id}</option>
-        `);
-    });
-}
+// Datos de los Niveles
+const formularioNiveles = document.querySelector('#myFormularioNiveles');
+formularioNiveles.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const data = Object.fromEntries(new FormData(e.target));
+  campus.addNivel(data);
+  console.log(campus.niveles);
+});
+
+// Datos del Roadmap
+const formularioRoadmaps = document.querySelector('#myFormularioRoadmaps');
+formularioRoadmaps.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const data = Object.fromEntries(new FormData(e.target));
+  campus.addRoadmap(data);
+  console.log(campus.roadmaps);
+});
